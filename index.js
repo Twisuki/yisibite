95f73974ce5a987b7252ac203598b9b05a5e9499
Su_Yang_233 <100500860+Scrohild@users.noreply.github.com>
2024-07-10 23:38:09 +0800

    Add files via upload

---
+++ index.js
@@ -0,0 +1,178 @@
+var typeList = [...list];
+var timeList = [...list];
+var videoCount = [];
+
+for (let i in list) {
+	videoCount[i] = false;
+}
+for (let i = 1; i < timeList.length; i++) {
+	for (let j = 0; j < i; j++) {
+		let j1 = parseInt(timeList[i].date.replace(/[^0-9]/g, ""), 10);
+		let j2 = parseInt(timeList[j].date.replace(/[^0-9]/g, ""), 10);
+		if (j1 > j2) {
+			let temp = timeList[i];
+			timeList[i] = timeList[j];
+			timeList[j] = temp;
+		}
+	}
+}
+for (let i = 1; i < typeList.length; i++) {
+	for (let j = 0; j < i; j++) {
+		let j1 = parseInt(typeSort(typeList[i].type));
+		let j2 = parseInt(typeSort(typeList[j].type));
+		if (j1 < j2) {
+			let temp = typeList[i];
+			typeList[i] = typeList[j];
+			typeList[j] = temp;
+		}
+	}
+}
+
+function typeSort(type) {
+	// type: "3-way", "eater", "gap", "block", "tunnel", "water", "quarry", "bedrock", "others"
+	// type:   三向    吞噬者   挖沟机  铺方块机   盾构机     排水机    采矿机     基岩机     其他
+	switch (type) {
+		case "3-way":
+			return 0;
+			break;
+		case "eater":
+			return 1;
+			break;
+		case "gap":
+			return 2;
+			break;
+		case "block":
+			return 3;
+			break;
+		case "tunnel":
+			return 4;
+			break;
+		case "water":
+			return 5;
+			break;
+		case "quarry":
+			return 6;
+			break;
+		case "bedrock":
+			return 7;
+			break;
+		case "others":
+			return 8;
+			break;
+	}
+}
+
+
+
+
+function indexList(i) {
+	if (i == 0) {
+		document.getElementById("t-0").className = "t-aft";
+		document.getElementById("t-1").className = "t-def";
+	} else {
+		document.getElementById("t-1").className = "t-aft";
+		document.getElementById("t-0").className = "t-def";
+	}
+}
+
+function timeLoad() {
+	for (let i in list) {
+		videoCount[i] = false;
+	}
+	
+	document.getElementById("table").innerHTML = "<div id='th'></div>";
+	document.getElementById("th").innerHTML = "<div id='th-td-0'>发布时间</div>" +
+		"<div id='th-td-1'>类型</div>" +
+		"<div id='th-td-2'>机器名称</div>" +
+		"<div id='th-td-3'>视频链接</div>" +
+		"<div id='th-td-4'>下载</div>";
+
+	for (let i in timeList) {
+		const rowHTML = `
+			<div class='tr'>
+				<div class='td-0'>${timeList[i].date}</div>
+				<div class='td-1'>${timeList[i].type}</div>
+				<div class='td-2'>${timeList[i].name}</div>
+				<div class='td-3' onclick='bilibili("${timeList[i].bv}", 0)'>${timeList[i].bv}</div>
+				<div class='td-4'>
+					<div class='btn btn-def' onclick='download("${timeList[i].bv}")'>下载</div>
+				</div>
+			</div>
+		`;
+		document.getElementById("table").innerHTML += rowHTML;
+	}
+}
+
+function typeLoad() {
+	for (let i in list) {
+		videoCount[i] = false;
+	}
+	
+	document.getElementById("table").innerHTML = "<div id='th'></div>";
+	document.getElementById("th").innerHTML = "<div class='td-0'>发布时间</div>" +
+		"<div class='td-1'>类型</div>" +
+		"<div class='td-2'>机器名称</div>" +
+		"<div class='td-3'>视频链接</div>" +
+		"<div class='td-4'>下载</div>";
+		
+		for (let i in typeList) {
+			const rowHTML = `
+				<div class='tr'>
+					<div class='td-0'>${typeList[i].date}</div>
+					<div class='td-1'>${typeList[i].type}</div>
+					<div class='td-2'>${typeList[i].name}</div>
+					<div class='td-3' onclick='bilibili("${typeList[i].bv}", 1)'>${typeList[i].bv}</div>
+					<div class='td-4'>
+						<div class='btn btn-def' onclick='download("${typeList[i].bv}")'>下载</div>
+					</div>
+				</div>
+			`;
+			document.getElementById("table").innerHTML += rowHTML;
+		}
+}
+
+function bilibili(givenBv, type) {
+	if (type == 0) {
+		for (let i in timeList) {
+			if (timeList[i].bv == givenBv) {
+				document.getElementsByClassName("btn")[i].className = "btn btn-aft";
+				break;
+			}
+		}
+		for (let i in list) {
+			if (list[i].bv == givenBv) {
+				videoCount[i] = true;
+				window.open("https://bilibili.com/video/" + list[i].bv);
+				break;
+			}
+		}
+	} else {
+		for (let i in typeList) {
+			if (typeList[i].bv == givenBv) {
+				document.getElementsByClassName("btn")[i].className = "btn btn-aft";
+				break;
+			}
+		}
+		for (let i in list) {
+			if (list[i].bv == givenBv) {
+				videoCount[i] = true;
+				window.open("https://bilibili.com/video/" + list[i].bv);
+				break;
+			}
+		}
+	}
+}
+
+function download(givenBv) {
+	for (let i in list) {
+		if (list[i].bv == givenBv) {
+			if (! videoCount[i]) {
+				window.alert("下载不可用\n请点击链接为可怜的月月贡献播放量");
+			} else {
+				let a = document.getElementById("downloader");
+				a.href = "litematica/" + list[i].bv + "." + list[i].extension;
+				a.click();
+			}
+		}
+	}
+}
---
+++ list.js
@@ -0,0 +1,67 @@
+// type: "3-way", "eater", "gap", "block", "tunnel", "water", "quarry", "bedrock", "others"
+// type:   三向    吞噬者   挖沟机  铺方块机   盾构机     排水机    采矿机     基岩机     其他
+window.list = [
+	{
+		"name": "单TNT轰炸机普通版",
+		"bv": "BV1hT4y1m7rY",
+		"date": "2022-01-14",
+		"type": "3-way",
+		"extension": "zip"
+	},
+	{
+		"name": "双TNT三向轰炸机",
+		"bv": "BV15Y4y1Y7TE",
+		"date": "2022-04-22",
+		"type": "3-way",
+		"extension": "rar"
+	},
+	{
+		"name": "双TNT自返回三向轰炸机",
+		"bv": "BV1w5411d7Fu",
+		"date": "2022-05-06",
+		"type": "3-way",
+		"extension": "rar"
+	},
+	{
+		"name": "单TNT轰炸机更好的开关",
+		"bv": "BV1Qg4y1y7Md",
+		"date": "2023-12-15",
+		"type": "3-way",
+		"extension": "zip"
+	},
+	{
+		"name": "三倍火力三向轰炸机",
+		"bv": "BV1C14y1b7mf",
+		"date": "2022-08-12",
+		"type": "3-way",
+		"extension": "zip"
+	},
+	{
+		"name": "无预留三向轰炸机",
+		"bv": "BV1xo4y167C2",
+		"date": "2023-03-10",
+		"type": "3-way",
+		"extension": "zip"
+	},
+	{
+		"name": "仅29个方块的极简双向复制三向轰炸机",
+		"bv": "BV1uJ4m1x75U",
+		"date": "2024-02-09",
+		"type": "3-way",
+		"extension": "zip"
+	},
+	{
+		"name": "1.7、3.3、6.7倍速三向轰炸机",
+		"bv": "BV1Ew411j71G",
+		"date": "2024-01-26",
+		"type": "3-way",
+		"extension": "zip"
+	},
+	{
+		"name": "1.111倍速下弹加速三向轰炸机",
+		"bv": "BV1y2421w7ga",
+		"date": "2024-02-02",
+		"type": "3-way",
+		"extension": "zip"
+	}
+];
