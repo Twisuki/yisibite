3ffec11ba62a5d41bd6a42c7e39ab43de9f2ab64
Nya_Twisuki <100500860+Twisuki@users.noreply.github.com>
2024-11-18 19:53:59 +0800

    Add files via upload

--- README.md
+++ README.md
@@ -1,20 +1,87 @@
-# 月月の投影存放处
-# Yisibite's litematica storage repositorie
+# 弦月档案
 
-这是一个存放B站up主火弦月的投影的库，在litematica文件夹里有每个视频对应的文件(zip或txt)
+由两个多月了, 我又重写了这个 _弦月档案_
 
-其中zip文件直接解压即可获得投影，txt则为该视频的投影文件过大导致无法传入Github
+实际上最初的 _弦月档案_ 只是个人的练习作品, 后来经 _Qubit_ ~~欺负~~ 指教, 改正成了上一版的样子
 
-This is a repositorie that store yisibite's litematica. In litematica folder, each file(zip or txt) referrence to a bilibili video
+上一版 _弦月档案_ 还有 _GreatFood404_ 做的圆角美化, _yuhan2680_ 帮忙写的 _README.md_
 
-You can unzip the zip file directly to get litematica, The txt file is because of the file of that video is too big so can't upload to Github
+但是现在又出现一些新的问题, 所以, 我直接 ~~删库跑路~~ 重写了这个项目
 
+## 文件
 
+1. > index.html 
+	
+	网页主入口, 包含 _#title_ , _#index_ , _#main_ , _#bottom_ 四部分
+	
+	1. > _div#title_
 
-为了防止有人无法登入Github, 本网站使用网络Fetch和仓库js两种加载方式
+		网页标题
+		
+	2. > _div#index_
+		
+		选择排序模式, 按时间排序 / 按类型排序
+		
+	3. > _div#main_
+		
+		主页面, 表头 + 打印的表格内容, 默认有一行占位
+		
+	4. > _div#bottom_
+		
+		夹带私货, 这次大胆把自己的信息写在上面了, 不怕你们修改
 
-To prevent somebody can't log in Github, this website use Internet Fetch and Repository js two loading way
+2. > index.css
+	
+	_index.html_ 配套 StyleSheet
 
-网站主体: Nya_Twisuki
+3. > index.js
+	
+	index.html 配套脚本
+	
+	1. > 默认流
 
-部分美化: GreatFood404
+		从 _list_ 读取数据, 填入 _timeList_ 和 _typeList_ 中, 并将 _timeList_ 和 _typeList_ 冒泡排序
+		
+	2. > _getTypeIndex()_
+			
+		输入_type_名称, 返回类型数值用于排序(可手动修改顺序)
+		
+	3. > _setIndex()_
+		
+		设置排序方式并实现
+		
+		先改变按钮颜色, 清空 _div#main-table_ , 从 _timeList_ 或 _typeList_ 中按顺序读取并填入
+		
+	4. > _genRow()_
+		
+		获取序号和对象, 读取对象数据, 向 _div#main-table_ 填入新行
+		
+	5. > _getTypeName()_
+		
+		输入_type_名称, 返回类型中文名用于显示
+		
+	6. > _playVideo()_
+		
+		打开B站播放对应视频
+		
+	7. > _getLitematica()_
+		
+		获取投影文件
+		
+		投影文件有三张获取方式, 直接下载, 大文件蓝奏云网盘下载, 接入杂鱼 _Qubit_ 的投影生成器
+
+4. > data.js
+
+	优先于 _index.js_ 加载, 存储 _list_
+	
+	对于_list_中对象详见 _data.js_ 开头的注释
+
+## Q&A
+
+1. > 为什么不使用json存储数据
+	
+	json文件无法直接读取, 若使用 _Fetch API_ 读取, 因网络问题很容易失败
+	
+2. > 为什么不使用两种读取方式
+	
+	上一版使用了两种读取方式, 基本上毫无作用
--- index.js
+++ index.js
@@ -1,194 +1,110 @@
-var list = [];
-var timeList = [];
-var typeList = [];
-var videoCount = [];
-var onDisplay;
-
-const litematicaGenerator = [
-	{
-		"id": "yisibite-world-eater",
-		"index": 12
-	},
-	{
-		"id": "yisibite-nether-eater",
-		"index": 14
-	},
-	{
-		"id": "yisibite-once-miner",
-		"index": 26
-	},
-	{
-		"id": "yisibite-3-miner",
-		"index": 27
-	},
-	{
-		"id": "yisibite-quarry-x",
-		"index": 29
-	}
-];
-
-loadListByFetch();
-function loadListByFetch() {
-	fetch("https://raw.githubusercontent.com/Scrohild/yisibite/main/index.json")
-		.then(response => response.json())
-		.then(data => {
-			list = data.list;
-			console.log("使用Fetch加载");
-			setList();
-			initVideoCount();
-			setIndexByTime();
-		})
-		.catch(error => {
-			window.alert("网络异常, 尝试使用js加载");
-			loadListByJs();
-			console.error("Fetch错误", error);
-		});
-}
-function loadListByJs() {
-	var script = document.createElement("script");
-	script.src = "data.js";
-	script.onload = function() {
-		if(typeof jsData !== "undefined") {
-			console.log("使用js加载");
-			list = jsData;
-			setList();
-			initVideoCount();
-			setIndexByTime();
-		} else {
-			window.alert("js出现错误, 请翻看黄历并改日再来");
-			console.error("js错误", error);
-		}
-	}
-	document.body.appendChild(script);
+// list元素写入序号, 排序时顺序改变, index最为唯一标识符
+for (let i in list) {
+	list[i].index = i
 }
 
-function setList() {
-	for (let i in list) {
-		list[i].index = i;
-	}
-	console.log(list);
-	timeList = [...list];
-	typeList = [...list];
-	
-	// 按时间排序
-	for (let i = 0; i < timeList.length - 1; i ++) {
-		for (let j = 0; j < timeList.length - i - 1; j ++) {
-			let a = parseInt(timeList[j].date.replace(/[^0-9]/g, ""), 10);
-			let b = parseInt(timeList[j + 1].date.replace(/[^0-9]/g, ""), 10);
-			
-			if (a < b) {
-				let temp = timeList[j];
-				timeList[j] = timeList[j + 1];
-				timeList[j + 1] = temp;
-			}
+var timeList = [...list];
+var typeList = [...list];
+
+// 按时间排序
+for (let i = 0; i < timeList.length - 1; i ++) {
+	for (let j = 0; j < timeList.length - i - 1; j ++) {
+		let a = parseInt(timeList[j].date.replace(/[^0-9]/g, ""), 10);
+		let b = parseInt(timeList[j + 1].date.replace(/[^0-9]/g, ""), 10);
+		
+		if (a < b) {
+			let temp = timeList[j];
+			timeList[j] = timeList[j + 1];
+			timeList[j + 1] = temp;
 		}
 	}
-	
-	// 按类型排序
-	for (let i = 0; i < typeList.length - 1; i ++) {
-		for (let j = 0; j < typeList.length - i - 1; j ++) {
-			let a = getTypeIndex(typeList[j]);
-			let b = getTypeIndex(typeList[j + 1]);
-			
-			if (a > b) {
-				let temp = typeList[j];
-				typeList[j] = typeList[j + 1];
-				typeList[j + 1] = temp;
-			}
-		}
-	}
-	
-	console.log(timeList);
-	console.log(typeList);
 }
+
+// 按类型排序
+for (let i = 0; i < typeList.length - 1; i ++) {
+	for (let j = 0; j < typeList.length - i - 1; j ++) {
+		let a = getTypeIndex(typeList[j]);
+		let b = getTypeIndex(typeList[j + 1]);
+		
+		if (a > b) {
+			let temp = typeList[j];
+			typeList[j] = typeList[j + 1];
+			typeList[j + 1] = temp;
+		}
+	}
+}
+// 获取类型对应排序数值
 function getTypeIndex(obj) {
 	switch (obj.type) {
-		case "3-way":return 0;
-		case "eater":return 1;
-		case "gap":return 2;
-		case "block":return 3;
-		case "tunnel":return 4;
-		case "water":return 5;
-		case "sand": return 6;
-		case "quarry":return 7;
-		case "bedrock":return 8;
-		case "others":return 9;
+		case "3-way" : return 0;
+		case "eater" : return 1;
+		case "gap" : return 2;
+		case "block" : return 3;
+		case "tunnel" : return 4;
+		case "water" : return 5;
+		case "sand" : return 6;
+		case "quarry" : return 7;
+		case "bedrock" : return 8;
+		case "others" : return 9;
 	}
 }
 
-function initVideoCount() {
-	if(localStorage.getItem("LSVideoCount") == null) {
-		for (let i in list) {
-			videoCount[i] = false;
-		}
-		localStorage.setItem("LSVideoCount", JSON.stringify(videoCount));
+// 设置排序方式
+function setIndex (type) {
+	// 改变按钮颜色
+	if (type == 0) {
+		document.getElementById("index-btn-time").className = "index-btn-pressed";
+		document.getElementById("index-btn-type").className = "index-btn";
 	} else {
-		videoCount = JSON.parse(localStorage.getItem("LSVideoCount"));
+		document.getElementById("index-btn-type").className = "index-btn-pressed";
+		document.getElementById("index-btn-time").className = "index-btn";
+	}
+	
+	// 清空div#main-table
+	document.getElementById("main-table").innerHTML = "";
+	
+	// 按排序方式填入HTML
+	if (type == 0) {
+		for (let i in timeList) {
+			genRow(i, timeList[i]);
+		}
+	} else {
+		for (let i in typeList) {
+			genRow(i, typeList[i]);
+		}
 	}
 }
 
-function setIndexByTime() {
-	onDisplay = 0;
-	// 改变颜色
-	document.getElementById("index-0").className = "index-btn-aft";
-	document.getElementById("index-1").className = "index-btn-def";
-	// 生成表格
-	document.getElementById("main-in").innerHTML = "";
-	for (let i in timeList) {
-		const rowHTML = `
-			<div class='main-row'>
-				<div class='main-index'>${i}</div>
-				<div class='main-time'>${timeList[i].date}</div>
-				<div class='main-type'>${getTypeName(timeList[i].type)}</div>
-				<div class='main-name'>${timeList[i].name}</div>
-				<div class='main-bv' onclick='playOnBilibili("${timeList[i].index}")'>视频</div>
-				<div class='main-download' onclick='download("${timeList[i].index}")'>下载</div>
-			</div>
-		`;
-		const fragment = document.createDocumentFragment();
-		const tempDiv = document.createElement('div');
-		tempDiv.innerHTML = rowHTML;
-		while (tempDiv.firstChild) {
-			fragment.appendChild(tempDiv.firstChild);
-		}
-		
-		document.getElementById("main-in").appendChild(fragment);
+function genRow (i, obj) {
+	const index = obj.index;
+	const date = obj.date;
+	const type = obj.type;
+	const name = obj.name;
+	const bv = obj.bv;
+	const rowHTML = `
+		<div class="main-row">
+			<div class="main-index">${+i + 1}</div>
+			<div class="main-time">${date}</div>
+			<div class="main-type">${getTypeName(type)}</div>
+			<div class="main-name">${name}</div>
+			<div class="main-video"><span onclick="playVideo(${index})"><img src="src/bilibili.png" /> 播放视频</span></div>
+			<div class="main-download"><span onclick="getLitematica(${index})">获取投影</span></div>
+		</div>
+	`;
+	
+	const fragment = document.createDocumentFragment();
+	const tempDiv = document.createElement('div');
+	tempDiv.innerHTML = rowHTML;
+	while (tempDiv.firstChild) {
+		fragment.appendChild(tempDiv.firstChild);
 	}
 	
-	setDownloadStyle();
+	document.getElementById("main-table").appendChild(fragment);
 }
-function setIndexByType() {
-	onDisplay = 1;
-	// 改变颜色
-	document.getElementById("index-0").className = "index-btn-def";
-	document.getElementById("index-1").className = "index-btn-aft";
-	// 生成表格
-	document.getElementById("main-in").innerHTML = "";
-	for (let i in typeList) {
-		const rowHTML = `
-			<div class='main-row'>
-				<div class='main-index'>${i}</div>
-				<div class='main-time'>${typeList[i].date}</div>
-				<div class='main-type'>${getTypeName(typeList[i].type)}</div>
-				<div class='main-name'>${typeList[i].name}</div>
-				<div class='main-bv' onclick='playOnBilibili(${typeList[i].index})'>视频</div>
-				<div class='main-download' onclick='download(${typeList[i].index})'>下载</div>
-			</div>
-		`;
-		const fragment = document.createDocumentFragment();
-		const tempDiv = document.createElement('div');
-		tempDiv.innerHTML = rowHTML;
-		while (tempDiv.firstChild) {
-			fragment.appendChild(tempDiv.firstChild);
-		}
-		
-		document.getElementById("main-in").appendChild(fragment);
-	}
-	
-	setDownloadStyle();
-}
-function getTypeName(givenType) {
-	switch (givenType) {
+
+// 获取类型对应名称
+function getTypeName (type) {
+	switch (type) {
 		case "3-way": return "三向轰炸机";
 		case "eater": return "世吞/地吞";
 		case "gap": return "挖沟机";
@@ -202,59 +118,22 @@ function getTypeName(givenType) {
 	}
 }
 
-function playOnBilibili(index) {
-	videoCount[index] = true;
-	localStorage.setItem("LSVideoCount", JSON.stringify(videoCount));
+function playVideo (index) {
 	window.open("https://bilibili.com/video/" + list[index].bv);
-	setDownloadStyle();
-}
-function setDownloadStyle() {
-	if (onDisplay == 0) {
-		for (let i in timeList) {
-			if(videoCount[timeList[i].index]) {
-				document.getElementsByClassName("main-download")[i].className = "main-download main-download-aft";
-				document.getElementsByClassName("main-download")[i].title = "点我下载";
-			} else {
-				document.getElementsByClassName("main-download")[i].className = "main-download main-download-def";
-				document.getElementsByClassName("main-download")[i].title = "下载不可用\n请点击链接为可怜的月月贡献播放量";
-			}
-		}
-	} else {
-		for (let i in typeList) {
-			if(videoCount[typeList[i].index]) {
-				document.getElementsByClassName("main-download")[i].className = "main-download main-download-aft";
-				document.getElementsByClassName("main-download")[i].title = "点我下载";
-			} else {
-				document.getElementsByClassName("main-download")[i].className = "main-download main-download-def";
-				document.getElementsByClassName("main-download")[i].title = "下载不可用\n请点击链接为可怜的月月贡献播放量";
-			}
-		}
-	}
 }
 
-function download(index) {
-	if(! videoCount[index]) {
-		window.alert("下载不可用\n请点击链接为可怜的月月贡献播放量");
-	} else if(getLitematicaGenerateYes(index)) {
-		let a = document.getElementById("downloader");
-		a.href = "litematica/" + list[index].bv + "." + list[index].extension;
-		a.click();
+function getLitematica (index) {
+	switch (list[index].remark[0]) {
+		case 0 :
+			let a = document.getElementById("downloader");
+			a.href = "litematica/" + list[index].bv + ".zip";
+			a.click();
+		break;
+		case 1 :
+			window.open(list[index].remark[1]);
+		break;
+		case 2 :
+			window.open("https://redenmc.com/zh_cn/litematica?m=" + list[index].remark[1]);
+		break;
 	}
-}
-function getLitematicaGenerateYes(index) {
-	for(let i in litematicaGenerator) {
-		if(litematicaGenerator[i].index == index) {
-			window.open("https://www.redenmc.com/mc-services/download/yisibite?m=" + litematicaGenerator[i].id);
-			return false;
-		}
-	}
-	return true;
-}
-
-function delLocalStorage() {
-	window.alert("你确定吗?\n此操作会清楚缓存, 且不可逆");
-	window.alert("你确定吗?\n这里会清除你的数据, 你需要重新观看视频才能下载");
-	window.alert("没关系, 咱不给你取消的机会^_^");
-	localStorage.removeItem('LSVideoCount');
-	location.reload();
 }
